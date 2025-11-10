c84570b0928252b06202cb89beee4c697f510195
Sumeet <sumeetmalloc@gmail.com>
2025-11-10 18:36:12 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,19 @@ import express from "express";
 import { ENV } from "./lib/env.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
-const app = express();
+import cors from "cors";
+import { serve } from "inngest/express";
+import { inngest } from "./lib/inngest.js";
 
+const app = express();
 const __dirname = path.resolve();
+
+// middleware
+app.use(express.json());
+// credentials:true ?? => server allows browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "Success from api" });
@@ -30,7 +40,7 @@ const startServer = async () => {
       console.log("server is running on port:", ENV.PORT);
     });
   } catch (error) {
-    console.log("Error starting the server",error)
+    console.log("Error starting the server", error);
   }
 };
 
